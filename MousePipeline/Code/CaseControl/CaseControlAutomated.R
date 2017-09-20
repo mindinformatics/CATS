7f86dff4a258fb0e1693eff30228f575c4f04ae1
Max <mlevinson1@harvard.mgh.edu>
2017-09-20 13:17:01 -0400

    Removed old pipeline files

--- MousePipeline/Code/CaseControl/CaseControlAutomated.R
+++
@@ -1,9 +0,0 @@
-# idea for the future
-CaseControlAutomated = function(GEOobject){
-  # examine pheno data for labeling
-  PhenoData = pData(phenoData(GEOobject))
-  
-  #
-  
-  return()
-}
--- MousePipeline/Download/remove.sh
+++
@@ -1,9 +0,0 @@
-# remove all of the GSE and GPL files from the directory
-rm GSE*
-rm GPL*
-
-# set Datasets.csv to the column names
-head -1 Datasets.csv > DatasetsCopy.csv
-rm Datasets.csv
-mv DatasetsCopy.csv Datasets.csv
-
